bcec1e7be3a33231c2a1ae5f74f50ca0163fa41d
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2019-01-29 09:36:45 +0100

    fourth commit

--- VO.py
+++ VO.py
@@ -1,10 +1,11 @@
 import argparse, os, csv #Importera modulerna 
 parser = argparse.ArgumentParser() #Bestämmer vad variabeln parser ska göra
-parser.add_argument("--file", "-f",type=str ,help="increase output file verbosity") #Specifierar vad cmdline ska kunna hantera via argument
+parser.add_argument("--file", "-f", type=str , help="increase output file verbosity") #Specifierar vad cmdline ska kunna hantera via argument
 args = parser.parse_args() #Returnerar data från filen vi specifierade med argumentet tidigare
-print(args.file) #Bestämmer vilken fil den ska ta ifrån
-with open(args.file, newline=" ") as csvfile: #
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row)
+print(args.file) #Bestämmer att den ska hämta från "file"
+with open(args.file, newline=" ") as csvfile: #Öppnar den tidigare specifierade "file" som en CSV-fil
+    reader = csv.DictReader(csvfile) #Använder reader funktionen för att läsa CSV-filen vi just öppnade
+    for row in reader: #För varje rad i filen...
+        print(row) #...Så skriver vi ut varje rad
+
 
