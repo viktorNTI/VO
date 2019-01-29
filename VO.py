ed84d6575230b1b49a27c6298b24678ee55a390f
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2019-01-29 10:58:44 +0100

    seventh commit

--- VO.py
+++ VO.py
@@ -1,4 +1,5 @@
-import argparse, os, csv #Importera modulerna 
+import argparse, os, csv, datetime #Importera modulerna 
+x = datetime.datetime.now()
 parser = argparse.ArgumentParser() #Bestämmer vad variabeln parser ska göra
 parser.add_argument("--file", "-f", type=str , help="increase output file verbosity") #Specifierar vad cmdline ska kunna hantera via argument
 args = parser.parse_args() #Returnerar data från filen vi specifierade med argumentet tidigare
@@ -6,8 +7,10 @@ print(args.file) #Bestämmer att den ska hämta från "file"
 with open(args.file, newline='') as csvfile: #Öppnar den tidigare specifierade "file" som en CSV-fil
     reader = csv.DictReader(csvfile) #Använder reader funktionen för att läsa CSV-filen vi just öppnade
     for row in reader: #För varje rad i filen...
-        print(row) #...Så skriver vi ut varje rad
-        #print(row['first_name'], row['last_name']) I "row" parantesen specifierar vi vad vi få ut från varje rad
+        #print(row) #...Så skriver vi ut varje rad
+        print(row['first_name'].lower()[0:3] + row['last_name'].lower()[0:3] + (str(x.year)[-2: ])) #I "row" parantesen specifierar vi vad vi vill få ut från varje rad
+
+
 
 
 #python VO.py --file students.csv 
